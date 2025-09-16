2d09e5d3f79da23830b041ac0963d658ffa1cd40
Kate Spencer <katemspencer529@gmail.com>
2025-09-16 09:34:41 -0500

    created layouts and more string stuff

--- app/src/main/java/com/example/pokedex/Linear_Layout.java
+++ app/src/main/java/com/example/pokedex/Linear_Layout.java
@@ -1,6 +1,7 @@
 package com.example.pokedex;
 
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +9,28 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class Linear_Layout extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.linear_layout);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
     }
+//    private void setupButtons(){
+//        resetButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//
+//                {
+//                }
+//            };
+//        }
+//    }
 }
+
